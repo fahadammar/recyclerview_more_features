7edc1db0c76c88e55141901626ded439a744637d
fahadammar <fahadammar96@outlook.com>
2023-02-08 17:08:22 +0500

    need to add the further onclick listener, the stuff in the adapter is done

--- app/src/main/java/com/example/recyclerviewwithfeatures/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/recyclerviewwithfeatures/adapter/RecyclerAdapter.kt
@@ -3,16 +3,31 @@ package com.example.recyclerviewwithfeatures.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ListView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewwithfeatures.databinding.ListLayoutBinding
 import com.example.recyclerviewwithfeatures.model.NewsModel
 
 class RecyclerAdapter(private val context: Context, private val data: ArrayList<NewsModel>) :
     RecyclerView.Adapter<RecyclerAdapter.ViewHolderClass>() {
-    class ViewHolderClass(binding: ListLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolderClass(binding: ListLayoutBinding, listener : OnItemClickListener) : RecyclerView.ViewHolder(binding.root) {
         val image = binding.titleImage
         val text = binding.showText
+
+        init {
+            binding.root.setOnClickListener {
+                // here we will place the click listener via the interface
+                listener.onItemClick(adapterPosition)
+            }
+        }
+    }
+
+    private lateinit var interfaceInstance : OnItemClickListener
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener){
+        interfaceInstance = onItemClickListener
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
@@ -21,7 +36,7 @@ class RecyclerAdapter(private val context: Context, private val data: ArrayList<
             parent,
             false
         )
-        return ViewHolderClass(binding)
+        return ViewHolderClass(binding, this.interfaceInstance)
     }
 
     override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
